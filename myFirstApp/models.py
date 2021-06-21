b291d105db69e4f5882cf82f45c709376a09579f
Roheeth41 <72456682+Roheeth41@users.noreply.github.com>
2021-06-21 09:02:37 +0530

    Models code

--- myFirstApp/models.py
+++ myFirstApp/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Vehicle(models.Model):
-    title = models.CharField(max_length = 200)
+    title = models.CharField(max_length = 200,unique=True)
+    types = models.CharField(max_length = 10)
     description = models.TextField()
 
     def __str__(self):
         return self.title
+
+class Sales(models.Model):
+    name = models.ForeignKey(Vehicle,on_delete=models.DO_NOTHING,)
+    company = models.CharField(max_length = 128)
+    year = models.CharField(max_length = 4)
+
+    def __str__(self):
+        return self.name
